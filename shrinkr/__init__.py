c00128f5b47e3d47ed231ed43a046a3471db5737
leosalgado <leonardosalgado@protonmail.com>
2025-10-26 02:36:28 -0300

    Create lib functions

--- shrinkr/__init__.py
+++ shrinkr/__init__.py
@@ -1,3 +1,26 @@
 from ._core import CompressorFactory, Compressor
 
-__all__ = ["CompressorFactory", "Compressor"]
+
+def compress(files, ctype=None):
+    if not files:
+        raise ValueError("No files provided")
+
+    if ctype is None:
+        ctype = "rle"
+
+    comp = CompressorFactory.create_by_name(ctype)
+    comp.compress(files)
+
+
+def decompress(files):
+    if not files:
+        raise ValueError("No files provided")
+
+    with open(files[0], "rb") as f:
+        first_byte = f.read(1)[0]
+
+    comp = CompressorFactory.create_by_id(first_byte)
+    comp.decompress(files)
+
+
+__all__ = ["compress", "decompress", "CompressorFactory", "Compressor"]
